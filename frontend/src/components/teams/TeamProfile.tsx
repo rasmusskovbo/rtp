2b0981a917bc8296cde06a26e9c11f4b4cf6f9c5
Rasmus Skovbo <rasmus.skovbo@shape.dk>
2023-08-08 12:47:33 +0200

    Improved teams layout

--- frontend/src/components/teams/TeamProfile.tsx
+++ frontend/src/components/teams/TeamProfile.tsx
@@ -11,6 +11,32 @@ const TeamProfile = ({ data }: { data: TeamData }) => {
                         <img className={styles["team-logo"]} src={data.teamLogo} alt="Team Logo" width="200" height="200" />
                         <img className={styles["owner-image"]} src={data.ownerImage} alt="Owner" width="200" height="200" />
                     </div>
+                    <Col className="d-flex flex-column justify-content-between">
+                        <div className={styles["team-name"]}>
+                            <span className={styles["label"]}>{data.teamName}</span>
+                        </div>
+                        <div className={styles["team-detail"]}>
+                            <span className={styles["label"]}>Team owner:</span>
+                            <span className={styles["value"]}>{data.ownerName}</span>
+                        </div>
+                        <div className={styles["team-detail"]}>
+                            <span className={styles["label"]}>Mascot:</span>
+                            <span className={styles["value"]}>{data.teamMascot}</span>
+                        </div>
+                        <div className={styles["team-detail"]}>
+                            <span className={styles["label"]}>Nationality:</span>
+                            <span className={styles["value"]}>Coming soon!</span>
+                        </div>
+                        <div className={styles["team-detail"]}>
+                            <span className={styles["label"]}>Years in the league:</span>
+                            <span className={styles["value"]}>{data.yearsInLeague}</span>
+                        </div>
+                        <div className={styles["team-bio"]}>
+                            <span>{data.bio}</span>
+                        </div>
+                    </Col>
+                </Col>
+                <Col>
                     <Card className={styles.card}>
                         <Card.Header>Season Stats</Card.Header>
                         <ListGroup variant="flush">
@@ -35,33 +61,8 @@ const TeamProfile = ({ data }: { data: TeamData }) => {
                                 {data.seasonStats.fpts_against}
                             </ListGroup.Item>
                         </ListGroup>
-
                     </Card>
-                </Col>
-                <Col>
-                    <Col className="d-flex flex-column justify-content-between">
-                        <div className={styles["team-name"]}>
-                            <span className={styles["label"]}>{data.teamName}</span>
-                        </div>
-                        <div className={styles["team-detail"]}>
-                            <span className={styles["label"]}>Team owner:</span>
-                            <span className={styles["value"]}>{data.ownerName}</span>
-                        </div>
-                        <div className={styles["team-detail"]}>
-                            <span className={styles["label"]}>Mascot:</span>
-                            <span className={styles["value"]}>{data.teamMascot}</span>
-                        </div>
-                        <div className={styles["team-detail"]}>
-                            <span className={styles["label"]}>Years in the league:</span>
-                            <span className={styles["value"]}>{data.yearsInLeague}</span>
-                        </div>
-                        <div className={styles["team-bio"]}>
-                            <span>{data.bio}</span>
-                        </div>
-                    </Col>
-
-
-                    <Card>
+                    <Card className={styles.card}>
                         <Card.Header>All Time Stats</Card.Header>
                         <ListGroup variant="flush">
                             <ListGroup.Item className={styles["stat-item"]}>
@@ -81,7 +82,19 @@ const TeamProfile = ({ data }: { data: TeamData }) => {
                                 {data.allTimeStats.allTimeRecord}
                             </ListGroup.Item>
                         </ListGroup>
-
+                    </Card>
+                    <Card className={styles.card}>
+                        <Card.Header>Rival</Card.Header>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item className={styles["stat-item"]}>
+                                <span className={styles["stat-label"]}>Rival Team:</span>
+                                Coming soon!
+                            </ListGroup.Item>
+                            <ListGroup.Item className={styles["stat-item"]}>
+                                <span className={styles["stat-label"]}>Head To Head Record</span>
+                                0-0
+                            </ListGroup.Item>
+                        </ListGroup>
                     </Card>
                 </Col>
                 <Col>
