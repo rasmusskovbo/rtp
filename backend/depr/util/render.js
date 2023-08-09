445f9e500feea23f9a1a889b8050a1f5efa358a2
Rasmus Skovbo <shape_rs@ok.dk>
2023-08-09 15:23:29 +0200

    Deleted old samples of code

--- backend/depr/util/render.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs"
-
-const nav = fs.readFileSync("./public_old/components/nav/nav.html", "utf8")
-const footer = fs.readFileSync("./public_old/components/footer/footer.html", "utf8")
-
-export function createPage(path, options) {
-    return (nav + fs.readFileSync(`./public/pages/${path}`, "utf8") + footer)
-            .replace("%%DOCUMENT_TITLE%%", options?.title || "Road To PInk")
-}
--- backend/depr/util/validation.js
+++
@@ -1,9 +0,0 @@
-const mailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-
-export function isEmailValid(email) {
-    return mailRegex.test(String(email).toLowerCase())
-}
-
-export function passwordsMatch(pw1, pw2) {
-    return pw1 === pw2
-}
